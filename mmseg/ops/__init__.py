2954c4793b592958c632420662f798070fe9f716
hakurei <155057697+HAKUREI629@users.noreply.github.com>
2025-08-25 10:53:25 +0800

    Add files via upload

---
+++ mmseg/ops/__init__.py
@@ -0,0 +1,4 @@
+from .encoding import Encoding
+from .wrappers import Upsample, resize, DualUpSample
+
+__all__ = ['Upsample', 'resize', 'Encoding', 'DualUpSample']
---
+++ mmseg/ops/wrappers.py
@@ -0,0 +1,93 @@
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def resize(input,
+           size=None,
+           scale_factor=None,
+           mode='nearest',
+           align_corners=None,
+           warning=True):
+    if warning:
+        if size is not None and align_corners:
+            input_h, input_w = tuple(int(x) for x in input.shape[2:])
+            output_h, output_w = tuple(int(x) for x in size)
+            if output_h > input_h or output_w > output_h:
+                if ((output_h > 1 and output_w > 1 and input_h > 1
+                     and input_w > 1) and (output_h - 1) % (input_h - 1)
+                        and (output_w - 1) % (input_w - 1)):
+                    warnings.warn(
+                        f'When align_corners={align_corners}, '
+                        'the output would more aligned if '
+                        f'input size {(input_h, input_w)} is `x+1` and '
+                        f'out size {(output_h, output_w)} is `nx+1`')
+    if isinstance(size, torch.Size):
+        size = tuple(int(x) for x in size)
+    return F.interpolate(input, size, scale_factor, mode, align_corners)
+
+
+class Upsample(nn.Module):
+
+    def __init__(self,
+                 size=None,
+                 scale_factor=None,
+                 mode='nearest',
+                 align_corners=None):
+        super(Upsample, self).__init__()
+        self.size = size
+        if isinstance(scale_factor, tuple):
+            self.scale_factor = tuple(float(factor) for factor in scale_factor)
+        else:
+            self.scale_factor = float(scale_factor) if scale_factor else None
+        self.mode = mode
+        self.align_corners = align_corners
+
+    def forward(self, x):
+        if not self.size:
+            size = [int(t * self.scale_factor) for t in x.shape[-2:]]
+        else:
+            size = self.size
+        return resize(x, size, None, self.mode, self.align_corners)
+
+class DualUpSample(nn.Module):
+    def __init__(self, 
+        in_channels, 
+        scale_factor):
+        super(DualUpSample, self).__init__()
+        self.factor = scale_factor
+
+        if self.factor == 2:
+            self.conv = nn.Conv2d(in_channels, in_channels, 1, 1, 0, bias=False)
+            self.up_p = nn.Sequential(nn.Conv2d(in_channels, 4*in_channels, 1, 1, 0, bias=False),
+                                      nn.PReLU(),
+                                      nn.PixelShuffle(scale_factor),
+                                      nn.Conv2d(in_channels, in_channels // 2, 1, stride=1, padding=0, bias=False))
+
+            self.up_b = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, 1, 0),
+                                      nn.PReLU(),
+                                      nn.Upsample(scale_factor=scale_factor, mode='nearest'),
+                                      nn.Conv2d(in_channels, in_channels // 2, 1, stride=1, padding=0, bias=False))
+        elif self.factor == 4:
+            self.conv = nn.Conv2d(2*in_channels, in_channels, 1, 1, 0, bias=False)
+            self.up_p = nn.Sequential(nn.Conv2d(in_channels, 16 * in_channels, 1, 1, 0, bias=False),
+                                      nn.PReLU(),
+                                      nn.PixelShuffle(scale_factor),
+                                      nn.Conv2d(in_channels, in_channels, 1, stride=1, padding=0, bias=False))
+
+            self.up_b = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, 1, 0),
+                                      nn.PReLU(),
+                                      nn.Upsample(scale_factor=scale_factor, mode='nearest'),
+                                      nn.Conv2d(in_channels, in_channels, 1, stride=1, padding=0, bias=False))
+
+    def forward(self, x):
+        """
+        x: B, L = H*W, C
+        """
+        x_p = self.up_p(x)  # pixel shuffle
+        x_b = self.up_b(x)  # bilinear
+        out = self.conv(torch.cat([x_p, x_b], dim=1))
+
+        return out
