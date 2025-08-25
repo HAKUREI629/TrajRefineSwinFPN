18731682ca2f1a988e54b086f5107d4e499a487e
hakurei <155057697+HAKUREI629@users.noreply.github.com>
2025-08-25 10:47:34 +0800

    Add files via upload

---
+++ configs/NGSwin/ngswin_base_patch4_window8_ngram2_512x512_160k_BTR_testCBF.py
@@ -0,0 +1,21 @@
+_base_ = [
+    '../_base_/models/NGramSwin.py', '../_base_/datasets/BTR_testCBF512.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=2))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optimizer = dict(type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+
+lr_config = dict(policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data=dict(samples_per_gpu=1)
